3703cf0d040b5c462d696b8a1e6a5f28ee79c642
Atharva262005 <atharvaralegankar2005@gmail.com>
2025-02-06 18:46:28 +0530

    vercel issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './fonts.css' // Add this import
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <AdminProvider>
+          <App />
+        </AdminProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
